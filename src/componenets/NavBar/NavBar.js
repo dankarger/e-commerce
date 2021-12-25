97acbb5b4f5ccc36f3fe96507515ba4f74da83e9
dankarger <karger.dan@gmail.com>
2021-12-25 14:37:34 +0200

    add local storage for cart

--- src/componenets/NavBar/NavBar.js
+++ src/componenets/NavBar/NavBar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './NavBar.css'
 import {Link} from "react-router-dom";
-import Cart from "../Cart/Cart";
 import Button from "../Button/Button";
 
 class NavBar extends React.Component {
